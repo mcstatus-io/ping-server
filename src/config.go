b6af68c4b5fc3fd6676e5c1f117d7db2d8a2f619
Chad Edwards <chad@saw.dev>
2023-03-24 00:09:10 -0400

    Added a new method overrideWithEnvVars to handle overriding configuration values with environment variables. Modified the ReadFile method to call overrideWithEnvVars after reading the configuration file and unmarshalling the YAML data. Added comments to the exported types and methods for better code documentation and understanding.

--- src/config.go
+++ src/config.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	"errors"
 	"os"
+	"strconv"
 	"time"
 
 	"gopkg.in/yaml.v3"
 )
 
+// CacheConfig contains cache-related settings.
 type CacheConfig struct {
 	JavaStatusDuration    time.Duration `yaml:"java_status_duration" json:"java_status_duration"`
 	BedrockStatusDuration time.Duration `yaml:"bedrock_status_duration" json:"bedrock_status_duration"`
 	IconDuration          time.Duration `yaml:"icon_duration" json:"icon_duration"`
 }
 
+// Config represents the application configuration.
 type Config struct {
 	Environment string      `yaml:"environment"`
 	Host        string      `yaml:"host"`
@@ -21,12 +25,37 @@ type Config struct {
 	Cache       CacheConfig `yaml:"cache"`
 }
 
+// ReadFile reads the configuration from the given file and overrides values using environment variables.
 func (c *Config) ReadFile(file string) error {
 	data, err := os.ReadFile(file)
-
 	if err != nil {
 		return err
 	}
 
-	return yaml.Unmarshal(data, c)
+	if err := yaml.Unmarshal(data, c); err != nil {
+		return err
+	}
+
+	return c.overrideWithEnvVars()
+}
+
+// overrideWithEnvVars overrides configuration values using environment variables.
+func (c *Config) overrideWithEnvVars() error {
+	if env := os.Getenv("ENVIRONMENT"); env != "" {
+		c.Environment = env
+	}
+
+	if host := os.Getenv("HOST"); host != "" {
+		c.Host = host
+	}
+
+	if port := os.Getenv("PORT"); port != "" {
+		portInt, err := strconv.Atoi(port)
+		if err != nil {
+			return errors.New("invalid port value in environment variable")
+		}
+		c.Port = uint16(portInt)
+	}
+
+	return nil
 }
